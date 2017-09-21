a9583c8b98770da2edb13468692dedf755a557c8
fabio rezende <fabio.rezende@gmail.com>
2017-09-20 22:04:58 -0300

    adicionado paginacao e calculo de paginas, falta poder escolher a pagina e atualizar automaticamente

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -9,6 +9,80 @@ namespace ControleEstoque.Web.Controllers
     public class CadastroController : Controller
     {
 
+        #region Grupo de Produtos
+
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            List<GrupoProdutoModel> lista = GrupoProdutoModel.RecuperarLista();
+
+            ViewBag.QuantMaxLinhasPorPagina = 5;
+            ViewBag.PaginaAtual = 1;
+
+            var difQuantPaginas = (lista.Count % ViewBag.QuantMaxLinhasPorPagina) > 0 ? 1 : 0;
+            ViewBag.QuantPaginas = (lista.Count / ViewBag.QuantMaxLinhasPorPagina) + difQuantPaginas;
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var id = model.Salvar();
+
+                    if (id > 0)
+                    {
+                        idSalvo = id.ToString();
+                    }
+                    else
+                    {
+                        resultado = "ERRO";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+
+            }
+
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            return Json(GrupoProdutoModel.ExcluirPeloId(id));
+        }
+
+        #endregion
+
         #region Usuarios
 
         private const string _senhaPadrao = ";13jaurb";
@@ -137,71 +211,6 @@ namespace ControleEstoque.Web.Controllers
         {
             return View();
         }
-
-        #region Grupo de Produtos
-
-        [Authorize]
-        public ActionResult GrupoProduto()
-        {
-            return View(GrupoProdutoModel.RecuperarLista());
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult RecuperarGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.RecuperarPeloId(id));
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
-        {
-            var resultado = "OK";
-            var mensagens = new List<string>();
-            var idSalvo = string.Empty;
-
-            if (!ModelState.IsValid)
-            {
-                resultado = "AVISO";
-                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
-            }
-            else
-            {
-                try
-                {
-                    var id = model.Salvar();
-
-                    if (id > 0)
-                    {
-                        idSalvo = id.ToString();
-                    }
-                    else
-                    {
-                        resultado = "ERRO";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    resultado = "ERRO";
-                }
-
-            }
-
-
-            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult ExcluirGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.ExcluirPeloId(id));
-        }
-
-        #endregion
+        
     }
 }
